c966cefb198bf3a1b89b32c9e39a8d7329853337
navintc <navin.thamindu@gmail.com>
2021-10-05 00:36:54 +0530

    Adding Home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Home = (props) =>{
+    return(
+        <div>
+            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                
+            </div>
+        </div>
+    );
+}
+
+export default Home;
