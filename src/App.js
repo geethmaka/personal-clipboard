c647be436a6daa3d02cdea67bd41d67aa9d271e2
navintc <navin.thamindu@gmail.com>
2021-10-05 13:23:05 +0530

    Wrote the skeletal frame

--- src/App.js
+++ src/App.js
@@ -1,15 +1,107 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Home from './pages/Home/Home';
 
-function App() {
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+
+import Card from '@mui/material/Card';
+import { Button, Typography } from '@mui/material';
+
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
+function NavBar() {
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <Home />
-      </header>
+    <div style={{display: "flex"}}>
+    {/* <Box
+      sx={{
+        display: 'flex',
+        width: '100%',
+        height: '100%',
+        // alignItems: 'center',
+        // justifyContent: 'center',
+        bgcolor: 'background.default',
+        color: 'text.primary',
+        borderRadius: 1,
+        p: 3,
+      }}
+    > */}
+      
+
+      {/* <Card> */}
+        <Typography variant='h1' className="topic1" color="text.primary">Personal-Clipboard</Typography>
+      {/* </Card>  */}
+
+      <Button onClick={colorMode.toggleColorMode} className="colorModeButton"sx={{height:"2.5rem"}}>
+        {theme.palette.mode} mode
+        <IconButton sx={{ ml: 1 }} color="inherit">
+          {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+        </IconButton>
+      </Button>
+      
+    {/* </Box> */}
     </div>
   );
 }
 
-export default App;
+
+
+
+export default function App() {
+  const [mode, setMode] = React.useState('light');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
+  return (
+    <div className="App" style={{height:"100%"}}>
+      <ColorModeContext.Provider value={colorMode} sx={{height:"100%"}}>
+        <ThemeProvider theme={theme} sx={{height:"50rem"}}>
+        <Box
+            sx={{
+              width: '100%',
+              height: '100%',
+              // alignItems: 'center',
+              // justifyContent: 'center',
+              bgcolor: 'background.default',
+              color: 'text.primary',
+              borderRadius: 1,
+              p: 3,
+            }}
+          >
+
+          <NavBar />
+          
+          <Home />
+
+          </Box>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </div>
+  );
+}
+
+
+
+
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import './Home.css';
+import Card from '@mui/material/Card';
 
+
+import { createTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from "@material-ui/core/styles";
+import { Typography, TextField, Button, Stack, Grid } from '@mui/material';
+
+const darkTheme = createTheme({
+  palette: {
+     mode: 'dark',
+  },
+});
 
 const Home = (props) =>{
     return(
         <div>
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                
+            
+            <TextField
+                id="outlined-multiline-static"
+                label="Multiline"
+                multiline
+                rows={4}
+                defaultValue="Default Value"
+            />
+
+            <div className="btn-stack-1">
+                <Stack spacing={2} direction="row">
+                    <Button variant="contained">Contained</Button>
+                    <Button variant="contained">Contained</Button>
+                </Stack>
             </div>
         </div>
     );
